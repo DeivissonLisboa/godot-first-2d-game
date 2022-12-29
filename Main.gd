c38cc638d71d7182e491f3a214b15caab14b86e5
Deivisson Lisboa <dvilaflor@gmail.com>
2022-12-29 03:53:23 -0300

    Change backgorund color, add music and shortcut key

--- Main.gd
+++ Main.gd
@@ -10,6 +10,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -17,6 +19,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_MobTimer_timeout():
